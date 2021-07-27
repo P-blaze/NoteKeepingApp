f36472d7b69cbc2b3717a55260a3e6d0cadff37f
P-blaze <poppiyush932@gmail.com>
2021-07-27 15:13:53 +0530

    deploy to firebase

---
+++ build/precache-manifest.d9b0cb368877b84d616535e8ae76953b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f330b63a3cfc8fbdb661934d9b10ce26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23918ac2b0fbc617e514",
+    "url": "/static/js/2.5c932d3d.chunk.js"
+  },
+  {
+    "revision": "bfd801d9a528439aafb5",
+    "url": "/static/js/main.2f63211d.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
